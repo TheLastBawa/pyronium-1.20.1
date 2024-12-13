5370e0a6e52149f51a04425f4465a9eab2c27a8a
TheLastBawa <jspouse.bawa@gmail.com>
2024-12-13 13:18:38 +0100

    Created Project

---
+++ src/main/java/net/the_last_bawa/pyronium/PyroniumClient.java
@@ -0,0 +1,10 @@
+package net.the_last_bawa.pyronium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PyroniumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/the_last_bawa/pyronium/PyroniumDataGenerator.java
@@ -0,0 +1,11 @@
+package net.the_last_bawa.pyronium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PyroniumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
